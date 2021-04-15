acce22969dd689f853cfaef57f88dcd7a74fc89e
Migaroez <geusens@gmail.com>
2021-04-15 19:21:44 +0200

    Basic vue app with web3/metamask integration working

--- site/src/main.js
+++ site/src/main.js
@@ -3,6 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// import npm dependencies
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 Vue.config.productionTip = false;
 
 new Vue({
--- site/src/views/Home.vue
+++ site/src/views/Home.vue
@@ -1,18 +1,70 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <section>
+    <div class="container text-light pt-5">
+        <div class="row">
+          <div class="col-md bg-gr-blue pr-5">
+                <h1>A DeFi platform,<br/> build for everyone</h1>
+                
+                <div class="my-5 py-3">
+                    <p class="mb-1">Projects vetted by professionals </p>
+                    <p class="mb-1">and brought to retail investors first.</p>
+                    <p class="mb-1">Powerful, easy-to-use, and lightining fast.</p>
+                </div>
+                
+                <button type="button" class="btn btn-dark btn-dark-blue">Get Started</button>
+          </div>
+          <div class="col-md pl-5">
+              <div class="token-statistics">
+                <h2 class="h1">Token Statistics</h2>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm p-3"><button type="button" class="btn btn-dark btn-dark-blue btn-block">Price:$</button></div>
+                        <div class="col-sm p-3"><button type="button" class="btn btn-dark btn-dark-blue btn-block">Mcap:</button></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm p-3"><button type="button" class="btn btn-light btn-block">Buy now</button></div>
+                        <div class="col-sm p-3"><button type="button" class="btn btn-light btn-block">Chart</button></div>
+                    </div>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+  </section>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 };
 </script>
+
+<style lang="scss">
+.landing-page-main-platform{
+    padding-top:120px;
+}
+.landing-page-main-statistics{
+    padding-top:120px;
+}
+
+.token-statistics{
+    margin-left: 20%;
+    margin-right: 20%;
+}
+
+.btn.btn-dark.btn-dark-blue{
+    background-color:rgb(0, 14, 48);
+    border-color:rgb(0, 14, 48);
+}
+
+.btn.btn-dark.btn-dark-blue:hover{
+    background-color: #343a40;
+    border-color: #343a40;
+}
+</style>
